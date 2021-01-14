7390d184a0f6700a5aacdd46fc3322537a931859
mauritio98 <57999694+mauritio98@users.noreply.github.com>
2021-01-14 16:45:46 +0100

    Initial commit

---
+++ main2.py
@@ -0,0 +1,22 @@
+import math as m
+# from math import sin
+
+
+def main():
+    """word = "python"
+    print(word[2])
+    print(word[-1])
+    print(len(word))
+    print(word[-6])
+    print(word[0:2])
+    print(word[2:])
+    print(word[2:6])
+    print(word)
+    # print(word[-1::-1])
+    print(word[::-1])"""
+
+    a = m.sin(45)
+
+
+if __name__ == '__main__':
+    main()
---
+++ venv/funcion1.py
@@ -0,0 +1,64 @@
+
+"""def my_function(**list):
+    print("La nota obtinguda a " +list["mp"]+ " és " + str(list["mark"]))
+
+def main():
+    my_function(mp="PB", mark=7)"""
+
+"""def main():
+    binario = 0
+
+    numero = int(input("Introduce un numero natural: "))
+
+    while numero > 128:
+        numero = int(input("Introduce un numero natural mas pequeño que 128: "))
+
+    resultado = conversion(numero)
+    print(resultado)
+
+def conversion(numero):
+    binario = 0
+    i=1
+    while numero > 0:
+        digito = numero % 2
+        binario = binario + digito * i
+        numero = numero//2
+        i = i * 10
+    return binario
+"""
+def main():
+    print("1. Sumar")
+    print("2. Restar")
+    print("3. Multplicar")
+    print("4. Dividir")
+    print("5. Salir")
+
+    opc = int(input("Introduce Opcion"))
+
+    while opc !=5:
+        opc = int(input("Introduce Opcion"))
+
+
+
+
+
+"""def main():
+    num1 = int(input("Introduce primer numero: "))
+    num2 = int(input("Introduce segundo numero: "))
+    num3 = int(input("Introduce tercer numero: "))
+
+    resultado = comprobar(num1,num2)
+    print("El mas grande es ",resultado)
+
+def comprobar(num1,num2):
+    if num1>num2:
+        return num1
+    return mayor(num1,num3)
+
+"""
+
+if __name__ == '__main__':
+    main()
+
+
+
