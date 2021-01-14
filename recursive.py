0188967a29bb5dd1aaf67c8fc6a2fd1ef3c2fead
mauritio98 <57999694+mauritio98@users.noreply.github.com>
2021-01-14 17:48:48 +0100

    cambio1

---
+++ recursive.py
@@ -0,0 +1,17 @@
+# factorial
+
+def
+
+
+
+
+def main():
+
+    numero = int(input("Introduce numero"))
+
+    print("El factorial del ",numero,"es :",factorial(numero))
+
+
+
+if __name__ == '__main__':
+    main()
