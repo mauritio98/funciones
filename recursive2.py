63db6e5751ab895d6ba07c3990921b5bb742955e
mauritio98 <57999694+mauritio98@users.noreply.github.com>
2021-01-14 18:13:21 +0100

    cambio2

--- recursive2.py
+++ recursive2.py
@@ -5,17 +5,14 @@ def fibonacci(n):
     if n == 0 or n == 1:
         return n
     else:
-        return fi
+        return fibonacci(n-1) + fibonacci(n-2)
 
 
 def main():
     numero = int(input("Introduce numero: "))
 
     for x in range(numero):
-        print(fibonacci(numero))
-        print(end=" ")
-
-
+        print(fibonacci(x), end=" ")
 
 
 
