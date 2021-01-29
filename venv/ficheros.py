0345d7a832510c074e70cbcf72653a856e1fac15
mauritio98 <57999694+mauritio98@users.noreply.github.com>
2021-01-29 16:16:49 +0100

    act 1,2,3 python

---
+++ venv/ficheros.py
@@ -0,0 +1,21 @@
+
+def fileRead(fname):
+    f = open(fname,"r")
+    print(f.readlines())
+    f.close()
+
+
+
+def main():
+    fileRead('C:/Users/alumne/Desktop/document.txt')
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
+
+
